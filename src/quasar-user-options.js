ac7ae065ca6d5ac9dc13de60ea8c746907092f4e
Sergey <owlbert@null.net>
2022-09-09 16:13:56 +0700

    add icons into project

--- src/quasar-user-options.js
+++ src/quasar-user-options.js
@@ -1,9 +1,14 @@
 import "quasar/dist/quasar.css";
 import "@quasar/extras/roboto-font/roboto-font.css";
 import "@quasar/extras/material-icons/material-icons.css";
-
+import "@quasar/extras/mdi-v7";
 // To be used on app.use(Quasar, { ... })
 export default {
   config: {},
   plugins: {},
+  extras: ["mdi-v7"],
+  framework: {
+    // svg-based example
+    iconSet: "svg-mdi-v7",
+  },
 };
